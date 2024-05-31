534d6fe9c863f46c0aaa7b95385452a794f5580c
Luke <luke890602@gmail.com>
2024-05-31 16:58:05 -0500

    add chain with tools

--- backend/app/server.py
+++ backend/app/server.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langserve import add_routes
 from pirate_speak.chain import chain as pirate_speak_chain
+from openai_functions_agent import agent_executor as openai_functions_agent_chain
 
 app = FastAPI()
+
 add_routes(app, pirate_speak_chain, path="/pirate-speak")
+add_routes(app, openai_functions_agent_chain, path="/openai-functions-agent")
 
 
 @app.get("/")
