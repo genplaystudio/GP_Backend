2104ac845e14cdca061cf1f63cc6a0e5a62d46cf
Geng Sun <gsun@iastate.edu>
2024-05-31 13:34:31 -0500

    init

---
+++ backend/app/server.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langserve import add_routes
+from pirate_speak.chain import chain as pirate_speak_chain
+
+app = FastAPI()
+add_routes(app, pirate_speak_chain, path="/pirate-speak")
+
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
